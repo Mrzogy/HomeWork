a70c903e26ad5473b22070dd6b3f59a15e8dc11d
mrzogy <magbolmrzogy@gmail.com>
2023-05-07 22:20:17 +0300

    Home works

---
+++ Lab - Constants and Variables.playground/Pages/3. Exercise - Variables.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+/*:
+## Exercise - Variables
+ 
+ Declare a variable `schooling` and set it to the number of years of school that you have completed. Print `schooling` to the console.
+ */
+var schooling = 18
+print(schooling)
+
+//:  Now imagine you just completed an additional year of school, and update the `schooling` variable accordingly. Print `schooling` to the console.
+ schooling += 1
+print(schooling)
+//:  Does the above code compile? Why is this different than trying to update a constant? Print your explanation to the console using the `print` function.
+let explanation = """
+As I explained on the previous page, the constant cannot be changed or modified,
+while the variable we can modify the value whenever we want to change it
+"""
+print(explanation)
+/*:
+[Previous](@previous)  |  page 3 of 10  |  [Next: App Exercise - Step Count](@next)
+ */
---
+++ Lab - Operators.playground/Pages/5. Exercise - Order of Operations.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+/*:
+## Exercise - Order of Operations
+
+ Print out what you think 10 + 2 * 5 evaluates to. Then print out the actual expression (i.e. `print(10 + 2 * 5)`)
+ */
+print(60)
+var how = 10 + 2 * 5
+print(how)
+
+//:  In a separate `print` statement, add in the necessary parentheses so that addition takes place before multiplication.
+var add = (10 + 2) * 5
+print(add)
+//:  Print out what you think 4 * 9 - 6 / 2 evaluates to. Then print out the actual expression.
+print(33)
+var multy = 4 * 9 - 6 / 2
+print(multy)
+
+//:  In a separate `print` statement, add in the necessary parentheses so that the subtraction is prioritized over the multiplication and division.
+var multy1 = 4 * (9 - 6) / 2
+print(multy)
+
+/*:
+[Previous](@previous)  |  page 5 of 8  |  [Next: App Exercise - Complex Fitness Calculations](@next)
+ */
